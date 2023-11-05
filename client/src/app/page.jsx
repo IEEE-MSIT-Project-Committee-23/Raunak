ba9f34a245b434c0256a1dd5b361ad1c4b96d0fb
Krrish <malekm09122003@gmail.com>
2023-11-05 17:36:58 +0530

    Added Gallery Carousel

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,16 +1,17 @@
 import About from '../components/About';
 import Partners from '../components/Partners';
 import Footer from '../components/common/footer';
+import Gallery from '../components/Gallery';
 
 const Home = () => {
   return (
     <div>
       <About/>
+      <Gallery />
       <Partners/>
       <Footer/>
     </div>
   )
 }
 
-export default Home
-
+export default Home;
---
+++ client/src/components/Gallery.jsx
@@ -0,0 +1,39 @@
+'use client'
+
+import 'jquery';
+import 'popper.js';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
+
+import React, { useEffect } from 'react';
+
+const Gallery = () => {
+    useEffect(() => {
+        $('.owl-carousel').owlCarousel({
+            items: 3,       
+            loop: true,        
+            margin: 16,        
+            autoplay:true,
+            autoplayTimeout:2000,
+        });
+        $('.owl-carousel').trigger('play.owl.autoplay',[2000]);
+    }, []);
+
+    return (
+        <section className="w-full bg-black/50 text-center px-12 py-6">
+            <h2 className="my-6 text-2xl font-bold text-white-800 md:mb-6 lg:text-3xl">Gallery</h2>
+            <div className="owl-carousel my-6">
+                <div className="item"><img src="./Gallery/img1.jpg" alt="Item 1" /></div>
+                <div className="item"><img src="./Gallery/img2.jpg" alt="Item 2" /></div>
+                <div className="item"><img src="./Gallery/img3.jpg" alt="Item 3" /></div>
+                <div className="item"><img src="./Gallery/img4.jpg" alt="Item 4" /></div>
+                <div className="item"><img src="./Gallery/img5.jpg" alt="Item 5" /></div>
+                <div className="item"><img src="./Gallery/img6.jpg" alt="Item 6" /></div>
+                <div className="item"><img src="./Gallery/img7.jpg" alt="Item 7" /></div>
+                <div className="item"><img src="./Gallery/img8.jpg" alt="Item 8" /></div>
+            </div>
+        </section>
+    );
+}
+
+export default Gallery;
