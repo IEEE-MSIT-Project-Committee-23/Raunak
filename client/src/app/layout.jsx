8237373ee25208b1ce29f6be9abe5b2672127923
UtkarshAhuja2003 <ahujautkarsh80@gmail.com>
2023-11-06 17:05:39 +0530

    added script for analytics

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import Navbar from '../components/common/navbar/index';
+import Script from "next/script";
 
 export const metadata = {
   title: "Raunak'23",
@@ -11,7 +12,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className = 'bg text'>
-    
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-MEY3XLGKFJ"></Script>
+      <Script>
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        
+        gtag('config', 'G-MEY3XLGKFJ');`}
+      </Script>
       <body>
       <Navbar />
       {children}</body>
